44e737bce61170d28091daae3dcc9e397feb1936
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-05-10 08:22:45 -0400

    Basic login and logout functionality using session storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Dashboard from "./views/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
+import useToken from "./hooks/useToken";
 
-function App() {
+const App = () => {
+  const { token, setToken } = useToken();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    console.log("token changed");
+    console.log(token);
+  }, [token]); //
+
+  if (!token) {
+    return <Login setToken={setToken} user={user} setUser={setUser} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+
+      <Dashboard setToken={setToken} user={user} />
     </div>
   );
-}
+};
 
 export default App;
